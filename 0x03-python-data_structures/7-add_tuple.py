c05bcc071ea2bb08e1bf0d4c94cd6807554c62e6
KarenNgugi <karenwngugi12@gmail.com>
2022-06-03 09:35:34 +0300

    initial commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    if len_a >= 2 and len_b >= 2:
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    else:
+        if len_b == 1:
+            return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+        elif len_b == 0:
+            return (tuple_a[0] + 0, tuple_a[1] + 0)
