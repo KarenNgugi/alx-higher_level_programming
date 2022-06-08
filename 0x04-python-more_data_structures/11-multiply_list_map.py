c3de6cac2f384ce363794a034325da98855ee2a9
KarenNgugi <karenwngugi12@gmail.com>
2022-06-08 15:57:13 +0300

    fixed task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if mylist is []:
-        return None
     def mult(num):
         return num * number
     new_list = map(mult, my_list)
