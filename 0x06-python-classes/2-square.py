385908202f9445b61e51ffdbcd35b5b473ebf4ec
KarenNgugi <karenwngugi12@gmail.com>
2022-06-21 16:03:08 +0300

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,13 +10,15 @@ class Square:
     """
     def __init__(self, size=0):
         try:
-            if size >= 0 and size.isdigit():
-                self.__size = size
-            elif size < 0 and size.isdigit():
-                raise ValueError
+            if str(abs(size)).isdigit():
+                try:
+                    if size >= 0:
+                        self.__size = size
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print("size must be >= 0")
             else:
                 raise TypeError
         except TypeError:
             print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
