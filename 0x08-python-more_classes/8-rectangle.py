9886c827d9d9f8d3164844b38af035372b2b30a2
KarenNgugi <karenwngugi12@gmail.com>
2022-06-27 16:43:18 +0300

    unfinished

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -88,3 +88,7 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         type(self).number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        pass
