cb1979980ec93be96bfb91c0b9334c7cdc735bd6
KarenNgugi <karenwngugi12@gmail.com>
2022-07-11 19:15:32 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""
-base module
-"""
+"""base module"""
 
 
 class Base:
     """
-    base class
+    Base class from which all other classes will be derived.
+
+    ...
+
+    Attributes
+    ----------
+    __nb_objects : int
+        global variable for number of instances
+
+    Methods
+    -------
+    to_json_string(list_dictionaries)
+        returns JSON string representation of list_dictionaries, \
+                returns "[]" if list_dictionaries is None
+
+    save_to_file(cls, list_objs)
+        writes the JSON string representation of list_objs to a file
+
     """
 
     global __nb_objects
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        class constructor
+        """
         global __nb_objects
         if id is not None:
             self.id = id
@@ -21,6 +39,9 @@ class Base:
             self.id = __nb_objects
 
     def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
         import json
 
         if list_dictionaries is None:
@@ -29,4 +50,7 @@ class Base:
             return json.dumps(list_dictionaries)
 
     def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+        """
         pass
