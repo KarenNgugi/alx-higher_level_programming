846dc9583cc2f81c209b0fc68474881b32653f1b
KarenNgugi <karenwngugi12@gmail.com>
2022-08-01 15:36:07 +0300

    updated task 4 test cases

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,6 +13,4 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print()
+        print("#" * size)
