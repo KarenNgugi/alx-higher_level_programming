c16c21029dced3c0d240ceaacd545d3879410d35
KarenNgugi <karenwngugi12@gmail.com>
2022-08-09 21:31:37 +0300

    task 12

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- updates Bob's score to 10 in table `second_table`
+-- you're not allowed to use Bob's id value, onlu the name field
+UPDATE TABLE `second_table` SET `score` = 10 WHERE `name` = "Bob";
