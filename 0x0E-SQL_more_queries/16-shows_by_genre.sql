efe0098e2bec00b6d50e201f1ffdbf9e623aedd4
KarenNgugi <karenwngugi12@gmail.com>
2022-08-10 20:23:18 +0300

    corrected task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@
 -- each record should display `tv_shows.title` `tv_genres.name`
 -- only one SELECT statement allowed
 SELECT `title`, `name` FROM `tv_shows`
-LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`genre_id`
+LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
 LEFT JOIN `tv_genres` ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
 ORDER BY `title`, `name`;
