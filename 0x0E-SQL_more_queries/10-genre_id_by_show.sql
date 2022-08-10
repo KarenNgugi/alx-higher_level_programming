39866445eb7545c80072c996baf1b792ea10bf31
KarenNgugi <karenwngugi12@gmail.com>
2022-08-10 19:39:58 +0300

    corrected task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@
 -- you can use only one SELECT statement
 SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
 FROM `hbtn_0d_tvshows`.`tv_shows`
-INNER JOIN `hbtn_0d_tvshows`.`tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`genre_id`
+INNER JOIN `hbtn_0d_tvshows`.`tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
 ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
