dab943a7082532aca8f9a07445a0373c8cf5aee4
KarenNgugi <karenwngugi12@gmail.com>
2022-08-10 19:33:04 +0300

    task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- list all shows contained in `hbtn_0d_tvshows` that have at least one genre
+-- each record should display `tv_shows.title` `tv_show_genres.genre_id`
+-- results must be sorted in ascending order by `tv_shows.title` and `tv_show_genres.genre_id`
+-- you can use only one SELECT statement
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `hbtn_0d_tvshows`.`tv_shows`, `hbtn_0d_tvshows`.`tv_show_genres`
+WHERE `tv_shows`.`id` = `tv_show_genres`.`genre_id`
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
