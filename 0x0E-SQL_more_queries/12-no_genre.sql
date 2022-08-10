85f949f12046972da0b34a16aafc32080b6f4b3b
KarenNgugi <karenwngugi12@gmail.com>
2022-08-10 19:54:10 +0300

    task 11

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- list all shows contained in `hbtn_0d_tvshows`
+-- list all shows contained in `hbtn_0d_tvshows` that don't have a genre
 -- each record should display `tv_shows.title` `tv_show_genres.genre_id`
 -- results must be sorted in ascending order by `tv_shows.title` and `tv_show_genres.genre_id`
 -- if a show does not have a genre, display NULL
@@ -6,4 +6,5 @@
 SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
 FROM `tv_shows`
 LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+WHERE `tv_show_genres`.`genre_id` IS NULL
 ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
