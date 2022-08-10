d08eccc26e86f7af1498a1453ed4710579127326
KarenNgugi <karenwngugi12@gmail.com>
2022-08-10 19:40:57 +0300

    corrected task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,6 @@
 -- results must be sorted in ascending order by `tv_shows.title` and `tv_show_genres.genre_id`
 -- you can use only one SELECT statement
 SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
-FROM `hbtn_0d_tvshows`.`tv_shows`
-INNER JOIN `hbtn_0d_tvshows`.`tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+FROM `tv_shows`
+INNER JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
 ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
