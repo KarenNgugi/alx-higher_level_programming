1e96d64cab7633f9cfb3f369625ffd8ace854f46
KarenNgugi <karenwngugi12@gmail.com>
2022-08-10 21:05:51 +0300

    corrected task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,11 +2,11 @@
 -- each record should display `tv_genres.name`
 -- results must be sorted in ascending order of genre name
 -- maximum 2 SELECT statements allowed
-SELECT `tv_genres`.`name` FROM `tv_genres`
-WHERE `tv_genres`.`id` NOT IN (
-	SELECT `tv_genres`.`id` FROM `tv_genres`
-	INNER JOIN `tv_show_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
-	INNER JOIN `tv_shows` ON `tv_shows_genres`.`show_id` = `tv_shows`.`id`
+SELECT `name` FROM `tv_genres`
+WHERE `name` NOT IN (
+	SELECT `name` FROM `tv_shows`
+	INNER JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+	INNER JOIN `tv_genres` ON `tv_shows_genre`.`genre_id` = `tv_genres`.`id`
 	WHERE `tv_shows`.`title` = "Dexter"
 )
-ORDER BY `tv_genres`.`name`;
+ORDER BY `name`;
